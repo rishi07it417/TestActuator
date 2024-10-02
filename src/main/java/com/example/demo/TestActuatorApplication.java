ad46aec3427715fbfac0dce25bc1e4c8989bd601
Rishi Pomal <38519496+rishi07it417@users.noreply.github.com>
2024-10-02 19:50:43 +0530

    initial commit

---
+++ src/main/java/com/example/demo/TestActuatorApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestActuatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestActuatorApplication.class, args);
+	}
+
+}
